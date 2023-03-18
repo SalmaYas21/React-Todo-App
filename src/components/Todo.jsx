88f7c2981f6f8f5610f21ca49fa20e9b8cc54ea7
Salma Yas <lamrisalmayasmine@gmail.com>
2023-03-18 11:28:36 +0100

    fixing the complete-todo problem

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -18,7 +18,7 @@ function Todo({todos, completeTodo, removeTodo, updateTodo}) {
 
   return todos.map((todo,index)=>(
     <div className={ todo.isComplete? 'todo-row complete' : 'todo-row' } key={index}>
-        <div key={todo.id} onClick={()=>{completeTodo(todo.id)}} >
+        <div key={todo.id} onClick={()=>{completeTodo(todo.id)}} className="todo-text">
             {todo.text}
         </div>
         <div className='icons'>
